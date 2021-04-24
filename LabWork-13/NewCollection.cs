3014538d1cbfbecdafd1975d78d4198952bd5ec6
Andrey Demenev <demenev2001@gmail.com>
2021-04-24 09:19:42 +0500

    init commit

---
+++ LabWork-13/NewCollection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LabWork_12;
+
+namespace LabWork_13
+{
+    class NewCollection<T> : MyCollection<T> where T: ICloneable, IComparable
+    {
+    }
+}
