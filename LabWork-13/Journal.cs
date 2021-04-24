fb36a1a706ddd2e89c36313cd6eea25caba3a836
Andrey Demenev <demenev2001@gmail.com>
2021-04-24 09:14:24 +0500

    Added new journal entities, overriding Show method

--- LabWork-13/Journal.cs
+++ LabWork-13/Journal.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using MyLibrary;
 using LabWork_12;
 
 namespace LabWork_13
@@ -15,27 +16,29 @@ namespace LabWork_13
         конструктор для инициализации полей класса; 
         перегруженную версию метода string ToString(). 
         всех элементах массива. 
-*/
+    */
     class JournalEntity : ICloneable, IComparable
     {
+        private DateTime EventTime { get; } = DateTime.Now;
+
         public string CollectionName { get; private set; } = "";
 
-        public string EventType { get; private set; } = "";
+        public string EventType { get; } = "Событие";
 
-        public string ChangedObjectInforation { get; private set; } = "";
+        public string ChangedObjectInformation { get; private set; } = "";
+
+        public JournalEntity(string collectionName, object changedObject)
+        {
+            CollectionName = collectionName;
+            EventType = "Событие";
+            ChangedObjectInformation = changedObject.ToString();
+        }
 
         public JournalEntity(string collectionName, string eventType, object changedObject)
         {
             CollectionName = collectionName;
             EventType = eventType;
-            ChangedObjectInforation = changedObject.ToString();
-        }
-
-        JournalEntity(string collectionName, string eventType, string changedObjectInformation)
-        {
-            CollectionName = collectionName;
-            EventType = eventType;
-            ChangedObjectInforation = changedObjectInformation;
+            ChangedObjectInformation = changedObject.ToString();
         }
 
         public override string ToString()
@@ -43,10 +46,12 @@ namespace LabWork_13
             return $"" +
                 $"Коллекция: {CollectionName}\n" +
                 $"Событие: {EventType}\n" +
-                $"Изменённый объект: {ChangedObjectInforation}";
+                $"Время: {EventTime}\n" +
+                $"Изменённый объект:\n" +
+                $"{ChangedObjectInformation}";
         }
 
-        public object Clone() => new JournalEntity(CollectionName, EventType, ChangedObjectInforation);
+        public object Clone() => new JournalEntity(CollectionName, EventType, ChangedObjectInformation);
 
         public int CompareTo(object obj)
         {
@@ -54,8 +59,47 @@ namespace LabWork_13
 
             if (CollectionName != temp.CollectionName) return CollectionName.CompareTo(temp.CollectionName);
             if (EventType != temp.EventType) return CollectionName.CompareTo(temp.CollectionName);
-            return ChangedObjectInforation.CompareTo(temp.ChangedObjectInforation);
+            return ChangedObjectInformation.CompareTo(temp.ChangedObjectInformation);
         }
     }
-    class Journal : MyCollection<JournalEntity> { }
+
+    class ChangedObjectJournalEntity : JournalEntity
+    {
+        public ChangedObjectJournalEntity(string collectionName, object changedObject) : base(collectionName, "Изменён объект по индексу", changedObject) { }
+    }
+
+    class RemovedObjectJournalEntity : JournalEntity
+    {
+        public RemovedObjectJournalEntity(string collectionName, object changedObject) : base(collectionName, "Из коллекции удалён объект", changedObject) { }
+    }
+
+    class AddedObjectJournalEntity : JournalEntity
+    {
+        public AddedObjectJournalEntity(string collectionName, object changedObject) : base(collectionName, "В коллекцию добавлен элемент", changedObject) { }
+    }
+
+    class Journal : MyCollection<JournalEntity>
+    {
+        public void Add(string collectionName, string eventType, object changedObject) => Add(new JournalEntity(collectionName, eventType, changedObject));
+
+        public void Add(ChangedObjectJournalEntity entity) => Add(entity);
+
+        public void Add(RemovedObjectJournalEntity entity) => Add(entity);
+
+        public void Add(AddedObjectJournalEntity entity) => Add((JournalEntity)entity);
+
+        public override void Show()
+        {
+            if (First != null)
+            {
+                CollectionPoint<JournalEntity> cursor = First;
+                while (cursor != null)
+                {
+                    Output.Text(cursor.Value);
+                    Output.Text("--\n");
+                    cursor = cursor.Next;
+                }
+            }
+        }
+    }
 }
