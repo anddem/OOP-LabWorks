5a9f2ed50cba98939bfeba72063bfa5787ad007e
Andrey Demenev <demenev2001@gmail.com>
2021-05-20 22:31:13 +0500

    Ready for Mark

--- LabWork-14/Program.cs
+++ LabWork-14/Program.cs
@@ -1,15 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
-using LabWork_10;
+﻿using LabWork_10;
 using LabWork_11;
 using MyLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LabWork_14
 {
-    internal class Program
+    internal static class Program
     {
+        private const ConsoleColor _LINQ_METHODS_RESULT = ConsoleColor.Green;
+        private const ConsoleColor _LINQ_QUERIES_RESULT = ConsoleColor.DarkBlue;
+        private const ConsoleColor _INFORMATION_COLOR = ConsoleColor.White;
+
         private static void Main(string[] args)
         {
             var collections = new TestCollections();
@@ -20,9 +23,22 @@ namespace LabWork_14
             FillList(continentList);
             FillDict(countryDict);
 
-            //PrintCollections(continentList, countryDict);
+            Output.Text("Это презентация работы LINQ запросов и методов расширений\n", _INFORMATION_COLOR);
+            Output.Text("Результаты работы запросов LINQ выделяются таким цветом: ", _INFORMATION_COLOR); 
+            Output.Text("■■■\n", _LINQ_QUERIES_RESULT);
+            Output.Text("А результаты работы методов расширений таким: ", _INFORMATION_COLOR);
+            Output.Text("■■■\n", _LINQ_METHODS_RESULT);
+            Output.Text(Environment.NewLine);
 
-            CountObjectsByType(continentList);
+            CountObjectsByType(continentList); // Счётчик, группировка
+
+            CountPopulation(continentList); // Агрегация
+
+            CountBuildingTypes(continentList); // Счётчик, группировка
+
+            OrderPlacesByPopulation(continentList, out int avgPopulation); // Сортировка по населению, агрегация
+
+            PrintCitiesWithPopulationMoreThanAverage(continentList, avgPopulation); // Выборка данных
         }
 
         private static void FillList(List<Place> list, int count = 50)
@@ -30,61 +46,23 @@ namespace LabWork_14
             for (var i = 1; i <= count; i++) list.Add(CreateRandomItem());
         }
 
-        private static void FillDict(SortedDictionary<Place, Region> dict, int count = 50)
+        private static void FillDict(SortedDictionary<Place, Region> dict, int count = 30)
         {
-            for (var i = 1; i <= 20; i++)
+            for (var i = 1; i <= count; i++)
             {
                 var region = CreateRandomRegion();
                 try
                 {
                     dict.Add(region.BasePlace, region);
                 }
-                catch (ArgumentException e)
+                catch (ArgumentException)
                 {
                     i--;
                 }
             }
         }
-
-
-
-        private static void CountObjectsByType(List<Place> list)
-        {
-            var places = (from place in list where place.GetType().ToString().Contains("Place") select place)
-                .Count();
-            var regions = (from region in list where region.GetType().ToString().Contains("Region") select region)
-                .Count();
-            var cities = (from city in list where city.GetType().ToString().Contains("City") select city)
-                .Count();
-            var addresses = (from address in list where address.GetType().ToString().Contains("Address") select address)
-                .Count();
-
-            Output.Success($"Places: {places}\n" +
-                           $"Regions: {regions}\n" +
-                           $"Cities: {cities}\n" +
-                           $"Addresses: {addresses}\n");
-        }
-
-        private static void PrintCollections(List<Place> list, SortedDictionary<Place, Region> dict)
-        {
-            PrintList(list);
-            PrintDict(dict);
-        }
-
-        private static void PrintList(List<Place> list)
-        {
-            foreach (var place in list) Output.Text($"{place.GetType()}\n{place}\n--\n");
-        }
-
-        private static void PrintDict(SortedDictionary<Place, Region> dict)
-        {
-            foreach (var pair in dict)
-            {
-                Output.Text(pair.Key, ConsoleColor.DarkBlue);
-                Output.Text($"{pair.Value}\n--\n", ConsoleColor.DarkMagenta);
-            }
-        }
-
+        
+        #region Генерация случайных элементов
         private static Place CreateRandomItem()
         {
             var type = Rand.Choice(new[] {"place", "region", "address", "city"});
@@ -125,7 +103,115 @@ namespace LabWork_14
                 out var population, out var houses, out var houseNumber);
             return new City(city, population, houses, new Address(name, street, houseNumber));
         }
+        #endregion
 
-        private static void CountPopulation()
+        private static void CountObjectsByType(List<Place> places)
+        {
+            var queryGroups = (from place in places group place by place.GetType()).ToDictionary(g => g.Key);
+            var methodsGroups = places.GroupBy(g => g.GetType()).ToDictionary(g => g.Key);
+
+            Output.Text("Подсчёт количества сгенерированных элементов по типам\n", _INFORMATION_COLOR);
+            Output.Text($"{"   Тип объекта    ",18} | Количество\n" +
+                        $"{new string('—', 32)}\n", _INFORMATION_COLOR);
+
+            foreach (var key in queryGroups.Keys)
+            {
+                Output.Text($"{key,-18} |  ", _INFORMATION_COLOR);
+                Output.Text($"{queryGroups[key].Count(),2}", _LINQ_QUERIES_RESULT);
+                Output.Text(" |  ", _INFORMATION_COLOR);
+                Output.Text($"{methodsGroups[key].Count(),2}\n", _LINQ_METHODS_RESULT);
+            }
+
+            Output.Text(Environment.NewLine);
+        }
+
+        private static void CountPopulation(List<Place> places)
+        {
+            // Операторы выборки
+            var pop1 = (from place in places
+                where place is Region && !(place is City)
+                select place).Sum(place => (place as Region)?.Population ?? 0);
+
+            // Методы расширения
+            var pop2 = places.Where(place => place is Region && !(place is City)).Sum(place => (place as Region)?.Population ?? 0);
+
+            Output.Text("Население в регионах:\n", _INFORMATION_COLOR);
+            Output.Text($"{pop1}", _LINQ_QUERIES_RESULT);
+            Output.Text(" | ", _INFORMATION_COLOR);
+            Output.Text($"{pop2}\n", _LINQ_METHODS_RESULT);
+
+            Output.Text(Environment.NewLine);
+        }
+
+        private static void CountBuildingTypes(List<Place> places)
+        {
+            var queryGroup = (from place in places
+                where place is Address
+                group place by (place as Address)?.Name).ToDictionary(group => group.Key);
+
+            var methodsGroups = places.Where(place => place is Address).GroupBy(place => (place as Address)?.Name)
+                .ToDictionary(group => group.Key);
+
+            Output.Text("Подсчёт зданий определённого типа\n", _INFORMATION_COLOR);
+            Output.Text($"{"    Тип здания    ",18} | Количество\n" +
+                        $"{new string('—', 32)}\n", _INFORMATION_COLOR);
+
+            foreach (var key in queryGroup.Keys)
+            {
+                Output.Text($"{key,-18} |  ", _INFORMATION_COLOR);
+                Output.Text($"{queryGroup[key].Count(),2}", _LINQ_QUERIES_RESULT);
+                Output.Text(" |  ", _INFORMATION_COLOR);
+                Output.Text($"{methodsGroups[key].Count(),2}\n", _LINQ_METHODS_RESULT);
+            }
+
+            Output.Text(Environment.NewLine);
+        }
+
+        private static void OrderPlacesByPopulation(List<Place> places, out int avgPopulation)
+        {
+            var queryOrdering = (from place in places orderby (place as Region)?.Population ?? 0 descending select place).ToList();
+
+            var methodsOrdering = places.OrderByDescending(place => (place as Region)?.Population ?? 0).ToList();
+
+            avgPopulation = (int)places.Where(place => place is Region).Average(place => (place as Region)?.Population);
+
+            Output.Text("Сортировка мест по населению\n", _INFORMATION_COLOR);
+            Output.Text(" №| Название места\n", _INFORMATION_COLOR);
+
+            for (int i = 0; i < queryOrdering.Count(); i++)
+            {
+                Output.Text($"{i + 1, 2}: ", _INFORMATION_COLOR);
+                Output.Text($"{queryOrdering.ElementAt(i).Name, -40}", _LINQ_QUERIES_RESULT);
+                Output.Text(" | ", _INFORMATION_COLOR);
+                Output.Text($"{methodsOrdering.ElementAt(i).Name, -40}", _LINQ_METHODS_RESULT);
+                Output.Text($" | Equal Objects: {queryOrdering.ElementAt(i).Equals(methodsOrdering.ElementAt(i))}", _INFORMATION_COLOR);
+                Output.Text($" | {(methodsOrdering.ElementAt(i) as Region)?.Population}\n", _INFORMATION_COLOR);
+            }
+
+            Output.Text(Environment.NewLine);
+        }
+
+        private static void PrintCitiesWithPopulationMoreThanAverage(List<Place> places, int avgPopulation)
+        {
+            var querySelect = (from place in places
+                where (place as Region)?.Population > avgPopulation
+                orderby (place as Region)?.Population descending
+                select place).ToList();
+
+            var methodsSelect = places.Where(place => (place as Region)?.Population > avgPopulation)
+                .OrderByDescending(place => (place as Region)?.Population).ToList();
+
+            Output.Text($"Регионы и города с населением, больше среднего ({avgPopulation})\n", _INFORMATION_COLOR);
+            Output.Text($"{new string('—', 32)}\n", _INFORMATION_COLOR);
+
+            for (int i = 0; i < querySelect.Count(); i++)
+            {
+                Output.Text($"{querySelect.ElementAt(i).Name,-40}", _LINQ_QUERIES_RESULT);
+                Output.Text(" | ", _INFORMATION_COLOR);
+                Output.Text($"{methodsSelect.ElementAt(i).Name}\n", _LINQ_METHODS_RESULT);
+            }
+
+            Output.Text(Environment.NewLine);
+        }
     }
 }
